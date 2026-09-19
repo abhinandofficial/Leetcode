a46c47f806cbd77ce27a7fc16f0aa73e0d0c2332
Abhinand C Varghese <86525541+abhinandofficial@users.noreply.github.com>
2026-09-19 11:30:42 +0530

    Time: 2 ms (81.68%) | Memory: 12.5 MB (40.68%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        closetoopen = { "}":"{" , ")":"(" , "]":"[" }
+        for i in s:
+            if i in closetoopen:
+                if stack and stack[-1] == closetoopen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return not stack
+
+     
